63aaffa1634d6bed12947ec73f2dbe0edf43a9b2
Lucas Martins <lmartins@tendencias.com.br>
2024-11-05 16:11:35 -0300

    Add error handling to login component and improve user feedback

--- components/LoginComponent.tsx
+++ components/LoginComponent.tsx
@@ -5,25 +5,37 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Link } from "expo-router";
 
 const LoginComponent = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
   const [isPasswordResetButtonPressed, setIsPasswordResetButtonPressed] =
     useState(false);
 
   const handleLogin = () => {
-    // Sua lógica de login aqui
+    const validEmail = "user"; // E-mail específico
+    const validPassword = "pass"; // Senha específica
+
+    if (email === validEmail && password === validPassword) {
+      setErrorMessage("");
+      setEmail("");
+      setPassword("");
+      router.push("/about"); // Navega para a página "/about"
+    } else {
+      setErrorMessage("E-mail ou senha incorretos");
+    }
   };
 
   const handleGoogleLogin = () => {
     // Implementar login com o Google posteriormente
   };
+
   const handlePasswordResetButtonPress = () => {
     setIsPasswordResetButtonPressed(!isPasswordResetButtonPressed);
   };
 
   return (
-    <View className="p-6 rounded-lg  w-full max-w-md">
+    <View className="p-6 rounded-lg w-full max-w-md">
       <TextInput
         className="border bg-white border-gray-700 rounded-2xl px-4 py-2 mb-4"
         placeholder="Insira seu e-mail"
@@ -37,6 +49,11 @@ const LoginComponent = () => {
         value={password}
         onChangeText={setPassword}
       />
+
+      {errorMessage ? (
+        <Text className="text-red-500 mb-4">{errorMessage}</Text>
+      ) : null}
+
       <View className="flex items-end">
         <TouchableOpacity
           className="text-blue-800 mb-4 underline"
@@ -46,26 +63,25 @@ const LoginComponent = () => {
         </TouchableOpacity>
       </View>
 
-      <Link href="/about" className="flex">
-        <TouchableOpacity
-          className="bg-blue-500 py-3 rounded-2xl mb-4 border border-gray-700 flex-1"
-          onPress={handleLogin}
-        >
-          <Text className="text-center text-white font-bold">Entrar</Text>
-        </TouchableOpacity>
-      </Link>
+      <TouchableOpacity
+        className="bg-blue-500 py-3 rounded-2xl mb-4 border border-gray-700 flex-1"
+        onPress={handleLogin}
+      >
+        <Text className="text-center text-white font-bold">Entrar</Text>
+      </TouchableOpacity>
 
       <TouchableOpacity
-        className="bg-white border  text-gray-600 border-gray-700 py-3 rounded-2xl mb-4 flex flex-row items-center justify-center"
+        className="bg-white border text-gray-600 border-gray-700 py-3 rounded-2xl mb-4 flex flex-row items-center justify-center"
         onPress={handleGoogleLogin}
       >
         <FontAwesome name="google" size={24} color="#DB4437" className="mr-2" />
-        <Text className="font-inter font-bold ">Continuar com Google</Text>
+        <Text className="font-inter font-bold">Continuar com Google</Text>
       </TouchableOpacity>
+
       <Text className="text-center p-4">
         Não tem conta?{" "}
         <TouchableOpacity className="text-blue-900 mb-4 underline">
-          Cadastre-se
+          <Text>Cadastre-se</Text>
         </TouchableOpacity>
       </Text>
     </View>
