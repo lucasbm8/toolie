ff54a9d54579e5bc1ebc13b7c76c9c2ce2c2ebb6
Lucas Martins <martinslucas2628@gmail.com>
2024-11-05 11:53:20 -0300

    Update react-native-safe-area-context version to 4.10.5

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif", "Inter_400Regular", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
