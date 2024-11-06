09622bdd7186f9a24b0fe165eff731d64e1c71e7
Lucas Martins <martinslucas2628@gmail.com>
2024-11-06 09:45:18 -0300

    Add custom color palette to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors: {
+        white: "#ffffff",
+        
+        // Primary color and its variations
+        "primary-light": "#DB6A5E",
+        primary: "#DB4437",
+        "primary-alt": "#A8321A",
+        "primary-dark": "#861E15",
+        "primary-clear": "#FAD4CE",
+        
+        // Secondary color and its variations (based on #53A3FA)
+        "secondary-clear": "#E6F3FE",
+        "secondary-light": "#9ACDFF",
+        secondary: "#53A3FA",
+        "secondary-alt": "#377DC4",
+        "secondary-dark": "#265A8A",
+
+        // Additional colors
+        grey: "#F2F2F2",
+        "grey-dark": "#333333",
+        blue: "#003366",
+      },
       fontFamily: {
         inter: ["Inter", "sans-serif", "Inter_400Regular", "sans-serif"],
       },
